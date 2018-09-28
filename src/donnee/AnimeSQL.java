b16e8a49b4fc51770a96f0c593eb4534bb8b673d
Tintou <martin.lm@outlook.fr>
2018-09-28 16:40:17 -0400

    public static final dans SQL

--- src/donnee/AnimeSQL.java
+++ src/donnee/AnimeSQL.java
@@ -2,9 +2,9 @@ package donnee;
 
 public interface AnimeSQL {
 	
-	String SQL_AJOUTER_ANIME = "INSERT into Anime(nom, nbEpisode, diffusion) VALUES(?,?,?)";
-	String SQL_RAPPORTER_ANIME = "SELECT * FROM anime WHERE id = ?";
-	String SQL_MODIFIER_ANIME = "UPDATE anime SET nom = ? , nbEpisode = ?, diffusion = ? WHERE id = ?";
-	String SQL_LISTER_ANIME = "SELECT * FROM anime";
+	public static final String SQL_AJOUTER_ANIME = "INSERT into Anime(nom, nbEpisode, diffusion) VALUES(?,?,?)";
+	public static final String SQL_RAPPORTER_ANIME = "SELECT * FROM anime WHERE id = ?";
+	public static final String SQL_MODIFIER_ANIME = "UPDATE anime SET nom = ? , nbEpisode = ?, diffusion = ? WHERE id = ?";
+	public static final String SQL_LISTER_ANIME = "SELECT * FROM anime";
 	
 }
--- src/donnee/StudioSQL.java
+++ src/donnee/StudioSQL.java
@@ -2,6 +2,6 @@ package donnee;
 
 public interface StudioSQL {
 	
-	String SQL_LISTER_STUDIO_PAR_ANIME = "SELECT * FROM studio WHERE anime = ?";
+	public static final String SQL_LISTER_STUDIO_PAR_ANIME = "SELECT * FROM studio WHERE anime = ?";
 
 }
